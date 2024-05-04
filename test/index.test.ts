136ef2ab6d1aa660da4700d28b4400ba2ab33bd7
mkpoli <git@mkpo.li>
2024-05-04 17:27:14 +0900

    Add basic test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,1 +1,19 @@
+import { test, expect } from 'bun:test';
 import { convertHangulToDPRK } from '../src/index';
+
+const TEST_CASES = [
+  {
+    hangul: '안녕하세요',
+    dprk: 'annyŏnghaseyo',
+  },
+  {
+    hangul: '반갑습니다',
+    dprk: 'pangapsumnida',
+  },
+];
+
+test('Hangul-DPRK', () => {
+  for (const { hangul, dprk } of TEST_CASES) {
+    expect(convertHangulToDPRK(hangul)).toEqual(dprk);
+  }
+});
